31253a462759cf6b655c824835476c71f788dac5
Andrii Kaluhin <nabupalassar@gmail.com>
2025-12-28 22:06:17 +0000

    Add back button to Settings page

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -7,29 +7,39 @@ import {
   IonItem,
   IonLabel,
   IonToggle,
+  IonButtons,     // 
+  IonBackButton,  // 
 } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonToggle],
+  imports: [
+    IonHeader, 
+    IonToolbar, 
+    IonTitle, 
+    IonContent, 
+    IonItem, 
+    IonLabel, 
+    IonToggle,
+    IonButtons,    // 
+    IonBackButton  // 
+  ],
 })
 export class SettingsPage {
-  // Default is true (Metric system)
   isMetric = true;
 
   constructor() {
     const saved = localStorage.getItem('unitsMetric');
-    // If nothing is saved (first run), default to true (Metric)
     if (saved !== null) {
       this.isMetric = saved === 'true';
     }
   }
 
   onUnitsChanged(ev: any) {
-    const isUS = ev.detail.checked; // If checked, user wants US units
-    this.isMetric = !isUS;          // So Metric becomes false
+    const isUS = ev.detail.checked;
+    this.isMetric = !isUS;
     localStorage.setItem('unitsMetric', String(this.isMetric));
   }
 }
